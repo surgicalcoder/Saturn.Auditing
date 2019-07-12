1522bcdd06da6964cfdbf3f139e936d0254386b6
Monty A <ma@surgicalcoder.com>
2019-07-12 18:02:21 +0200

    Adding in audit provider abstraction

---
+++ GoLive.Saturn.Auditing.AuditProvider/Registration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using GoLive.Saturn.Data.Abstractions;
+using MongoDB.Driver;
+
+namespace GoLive.Saturn.Auditing.AuditProvider
+{
+    public static class Registration
+    {
+        public static void Register(RepositoryOptions options, string collectionName = "Audit")
+        {
+            if (string.IsNullOrWhiteSpace(collectionName))
+            {
+                throw new ArgumentException("Value cannot be null or whitespace.", nameof(collectionName));
+            }
+
+            var mongoUrl = new MongoUrl(options.ConnectionString);
+
+            Audit.Core.Configuration.DataProvider = new Audit.MongoDB.Providers.MongoDataProvider()
+            {
+                ConnectionString = mongoUrl.Url,
+                Database = mongoUrl.DatabaseName,
+                Collection = collectionName
+            };
+        }
+    }
+}
